007b6f50b54c8b1af84ab33f7a830675b17f2988
brunoleite <bmarianoleite3@gmail.com>
2022-04-10 19:13:45 +0000

    generage code, take guess and correction

--- mastermind.rb
+++ mastermind.rb
@@ -1,29 +1,46 @@
+require 'pry'
+
 class Mastermind
   def initialize(player)
     @player = player
-    @current_guess = [1, 2, 3, 4]
-    @correction = ["right", "almost"]
-    @gusses = {}
-    @colors = %w[vermelho azul verde amarelo marrom laranja preto branco]
+    @code = []
+    @code_gusses = []
+    @correction = []
   end
 
-  def color_choices
-    colors_size = @colors.length - 1
-    @colors.each_with_index do |color, idx|
-      if idx == colors_size
-        print "#{color}\n"
-      else
-        print "#{color} | "
+  def generate_code
+    while @code.size < 4
+      rand_num = rand(0..8)
+      unless @code.any?{ |num| num == rand_num }
+        @code << rand_num
       end
     end
-    puts "    1    |   2  |   3   |    4    |    5   |    6    |   7   |    8"
+    p @code
   end
 
-  def display_game
-    puts "#{@current_guess} #{@correction}"
+  def take_a_guess(current_code_guess)
+    current_code_guess = current_code_guess.split('')
+    if current_code_guess.size == 4
+      @current_code_guess = current_code_guess.uniq
+    end
+    p @current_code_guess
+  end
+
+  def correction
+    @current_code_guess.each_with_index do |guess_num, guess_idx|
+      @code.each_with_index do |code_num, code_idx|        
+        if guess_num.to_i == code_num && guess_idx == code_idx
+          @correction << 2
+        elsif guess_num.to_i == code_num
+          @correction << 1
+        end
+      end
+    end
+    p @correction
   end
 end
 
 game = Mastermind.new('bruno')
-game.color_choices
-game.display_game
+game.generate_code
+game.take_a_guess("1342")
+game.correction
